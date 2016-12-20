e93bfc076f4d5e80f40ea27e613498bbfb7e882c
ChauLui <Chau.Lui@gmail.com>
2016-12-20 18:39:50 -0500

    solution filled in

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to make use of scoping rules in R to cache matrix inverse values
+## rather re-calculation values when inital matrix remains the same.
 
-## Write a short comment describing this function
+## this function provides up the set/get functions for the special 
+## matrix object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invM = NULL
+  set <- function(y){
+    x <<- y
+    invM <<- NULL
+  }
+  get <- function()x
+  setInvM <- function(inv) invM <<- inv
+  getInvM <- function() invM
+  list(set=set, get=get, setInvM=setInvM, getInvM=getInvM)
 }
 
 
-## Write a short comment describing this function
+## This function checks if inverse matrix was calculated.
+## If so, retrieve inverse matrix; otherwise calculates inverse matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invcM <-x$getInvM()
+  if (!is.null(invcM)){
+    message("getting cached inverse matrix")
+    return (invcM)
+  }
+  data <- x$get()
+  invcM <- solve(data, ...)
+  x$setInvM(invcM)
+  invcM
 }
